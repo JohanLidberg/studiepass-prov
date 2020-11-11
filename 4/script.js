13502954907b3a4dbc94fa4e8eb866ca55447ad1
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-11-11 13:59:16 +0100

    4

--- 4/script.js
+++ 4/script.js
@@ -1,9 +1,32 @@
 // 1. Skapa en variabel som innehåller en array med fyra strängar, t.ex. fyra namn på personer.
+const cars = ["bmw", "volvo", "saab", "bugati"]
 
 // 2. Logga ut arrayen.
+console.log(cars)
 
 // 3. Byt ut det andra elementet till något annat.
+//jag väljer att byta ut sista elemntet.
+function Ändra(arr) {
+    return arr[arr.length -1] = ändring
+
+}
+const cars = ["bmw", "volvo", "saab", "bugati"]
+
+
+console.log(cars)
 
 // 4. Logga ut arrayen igen.
 
 // 5. Logga ut det sista elementet i arrayen.
+function last(arr) {
+    return arr[3]
+}
+const cars = ["bmw", "volvo", "saab", "bugati"]
+
+console.log(last(cars))
+
+
+
+
+
+
