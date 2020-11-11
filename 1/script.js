68d2cf747416330b219dede4fae203c456a37c28
Johan Lidberg <johan.lidberg@skola.botkyrka.se>
2020-11-11 13:25:55 +0100

    1

--- 1/script.js
+++ 1/script.js
@@ -1,25 +1,32 @@
 
 // 1. Deklarera en variabel a och tilldela den värdet 5. Byt sedan värdet på a till 8
+conts a = 5
+myFunction()
+
+function myFunction(){
+   a = 20
+}
 
 // 2. Deklarera en konstant variabel tilldela den värdet av ditt förnamn
+const förnamn = ("Lidberg")
 
 // 3. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: _1___
+// b) Vilken datatyp returnerar den? SVAR: _string____
 function foo() {
     return "foo"
 }
 
 // 4. 
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: _1___
+// b) Vilken datatyp returnerar den? SVAR: __string___
 function bar(arr) {
     return arr
 }
 
 // 5.
-// a) Hur många argument tar den här funktionen? SVAR: ____
-// b) Vilken datatyp returnerar den? SVAR: _____
+// a) Hur många argument tar den här funktionen? SVAR: __1__
+// b) Vilken datatyp returnerar den? SVAR: string_____
 function bar(a, b, c) {
     console.log(a)
 }
--- 2/script.js
+++ 2/script.js
@@ -1,12 +1,19 @@
 
 // 1. Varför loggas ingenting ut när man kör det här skriptet? 
-// Det finns två fel. SVAR: ________
+// Det finns två fel. SVAR: __först hara hen inga citatatecken på det du vill skriva ut.Sen har functionern ingen namn,det ska ståå console.log(namne("hello Hello, how are you today?")____
 
 // 2. Ändra scriptet så att "Hello, how are you today?" verkligen loggas ut.
 
 function hello() {
-    console.log(Hello, how are you today?)
+    console.log("Hello, how are you today?")
 }
+
+//så hör skulle jag skriva 
+function hello(str) {
+    return str
+}
+console.log(hello("Hello, how are you today?"))
 
 // 3. Uppdatera funktionen hello så att den tar ett namn som argument
 // Gör så att den hälsar med namn, t.ex. "Hello Fredrik, how are you today?"
+
